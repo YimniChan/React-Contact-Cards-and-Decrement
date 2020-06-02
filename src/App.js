006b45ff99233bfa420fbfa134ff05d2181b0b5e
Yimni Chan <ynchan818@gmail.com>
2020-06-02 15:52:35 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,49 @@
+import React, { Component } from "react";
+import ContactCard from "./ContactCard";
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return (
+      <>
+        <ContactCard
+          name="John"
+          mobile="2125555555"
+          work="8002222222"
+          email="hello@example.com"
+        />
+        <ContactCard
+          name="Jill"
+          mobile="7185555555"
+          work="7182222222"
+          email="jill@example.org"
+        />
+        <ContactCard
+          name="Mike"
+          mobile="3475555555"
+          work="5552222222"
+          email="mike@example.com"
+        />
+      </>
+    );
+  }
+}
+
+
+/**Deccreament
+ * class App extends Component {
+  render() {
+    return (
+      <>
+        <Decrement start={5} />
+        <Decrement start={15} />
+        <Decrement start={50} />
+      </>
+    );
+  }
+}
+ * 
+ * 
+ */
+export default App;
+
